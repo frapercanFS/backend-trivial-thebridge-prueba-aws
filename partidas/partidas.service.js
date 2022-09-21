0c28e6a97693e78920572c23a8c2f8474eb42441
idavid80 <dominguezbueno.david@gmail.com>
2022-09-21 13:35:57 +0200

    postman partidas ok, falta partidas-categoria

--- partidas/partidas.service.js
+++ partidas/partidas.service.js
@@ -5,28 +5,28 @@ const Categoria = db.Categorias;
 module.exports = {
   getAll,
   create,
-  getPartidasByCategory,
+  getPartidasByCategory
 };
 
 async function getAll() {
-  return await Partidas.find().sort({puntuacion:-1})
+  return await Partidas.find()
 }
 
 async function create(body) {
   let partidas = new Partidas(body);
+  console.log(partidas)
   return await partidas.save();
-}
+  }
 
-/* async function getPartidasByCategory(body) {
-  const categoria = await Categoria.findOne({ nombre: body.categoria });
-  console.log("partida",categoria)
-  return await Partidas.find({ categoria }).limit(5).sort({puntuacion:-1})
-}  */
+/*   async function getPartidasByCategory (body) {
+    const categoria = await Categoria.findOne({ nombre: body.categoria })
+    return await Partidas.find({ categoria }).limit(5)
+  }
+ */
 
-
-async function getPartidasByCategory(body) {
-  const categoria = await Categoria.findOne({ nombre: body.categoria });
-  console.log("categoria-partida",categoria)
-  return await Partidas.find({categoria}).populate('categoria').limit(5)
-    
-} 
+  async function getPartidasByCategory(body) {
+    const categoria = await Categoria.findOne({ nombre: body.categoria });
+    console.log("categoria",categoria)
+    return await Partidas.find({categoria}).populate('categoria').limit(5)
+      
+  } 
