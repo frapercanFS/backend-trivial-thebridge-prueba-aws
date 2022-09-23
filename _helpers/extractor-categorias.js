9f240423921b256f2230f5aa84b53ed7a8ec3a85
franalvarez88 <104025411+franalvarez88@users.noreply.github.com>
2022-09-23 20:15:22 +0200

    categoria masiva

--- _helpers/extractor-categorias.js
+++ _helpers/extractor-categorias.js
@@ -1,19 +1,24 @@
-const db = require("../_helpers/db");
-const Categoria = db.Categorias;
+const translate = require('translate-google')
+const db = require('../_helpers/db')
+const Categoria = db.Categorias
 
 const fetch = (url) =>
-import("node-fetch").then(({ default: fetch }) => fetch(url));
+  import('node-fetch').then(({ default: fetch }) => fetch(url))
 
-
-fetch(
-"https://opentdb.com/api_category.php"
-)
-.then((response) => response.json())
-.then((data) =>
-  data.trivia_categories.map((categoria) => {
-    let categoriaTransformada = {nombre:{es:undefined,en:categoria.name}}
-    let categoriaInsertar = new Categoria(categoriaTransformada)
-    categoriaInsertar.save()
-  })
-)
-
+fetch('https://opentdb.com/api_category.php')
+  .then((response) => response.json())
+  .then((data) =>
+    data.trivia_categories.map((categoria) => {
+      translate(categoria.name, { to: 'es' })
+        .then((categoriaTraducida) => {
+          const categoriaTransformada = {
+            nombre: { es: categoriaTraducida, en: categoria.name }
+          }
+          const categoriaInsertar = new Categoria(categoriaTransformada)
+          categoriaInsertar.save()
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    })
+  )
